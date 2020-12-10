fca94a5539e24eb90d8adeba0be6cd7d1d500d4d
Dorn <dornkholland@gmail.com>
2020-12-09 17:08:51 -0700

    completed functionality of promise button

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
+import getClueFromPromise from "./promise-version.js"
 window.addEventListener('DOMContentLoaded', () => {
-    getClue().then(res => console.log(res))
+    // button click add event listener
+    document.getElementById('use-promise').addEventListener('click', () => {
+        getClueFromPromise().then(res => {
+            console.log(res);
+            document.getElementById('question').innerHTML = res.question;
+            document.getElementById('answer').innerHTML = res.answer;
+            document.getElementById('value').innerHTML = res.value;
+            document.getElementById('category-title').innerHTML = res.category.title;
+            document.getElementById('invalid-count').innerHTML = res.invalidCount > 0 ? 'invalid' : 'valid';
+    })
 })
+})
+
+// ta questions:
+// game.js line 10 is res.category.title bad practice
